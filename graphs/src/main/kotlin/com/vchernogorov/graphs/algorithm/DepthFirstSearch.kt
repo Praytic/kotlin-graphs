44c6af5363b3a386a3bafb77a5b3592e099ca81f
Praytic <chernogorovvm@gmail.com>
2016-10-16 16:51:48 +0300

    Add DepthFirstSearch#onExitVisitedVertex()

--- graphs/src/main/kotlin/com/vchernogorov/graphs/algorithm/DepthFirstSearch.kt
+++ graphs/src/main/kotlin/com/vchernogorov/graphs/algorithm/DepthFirstSearch.kt
@@ -6,32 +6,37 @@ import com.vchernogorov.graphs.storage.Vertex
 
 internal class DepthFirstSearch<T>(val graph: Graph<T>,
                                    val startVertex: Vertex<T>,
-                                   var onFindUnvisitedVertex: (Vertex<T>, Edge<T>?) -> Unit,
-                                   var onFindVisitedVertex: (Vertex<T>, Edge<T>) -> Unit):
+                                   var onEnterUnvisitedVertex: (Vertex<T>, Edge<T>?) -> Unit,
+                                   var onFindVisitedVertex: (Vertex<T>, Edge<T>) -> Unit,
+                                   var onExitVisitedVertex: (Vertex<T>, Edge<T>?) -> Unit):
         Algorithm<Unit>(Unit) {
     constructor(graph: Graph<T>): this(graph, graph.vertices.first())
     constructor(graph: Graph<T>, startVertex: Vertex<T>): this(graph, startVertex,
-            { vertex, edge -> }, { vertex, edge -> })
+            { vertex, edge -> }, { vertex, edge -> }, { vertex, edge -> })
 
     override fun run() {
         if (!graph.vertices.isNotEmpty()) return
         if (graph.getOutgoingEdges(startVertex).isEmpty()) {
-            onFindUnvisitedVertex(startVertex, null)
+            onEnterUnvisitedVertex(startVertex, null)
             return
         }
         val visitedVertices = graph.getVisitedMap()
         fun recursiveDfs(currentVertex: Vertex<T>, currentEdge: Edge<T>?) {
             visitedVertices[currentVertex] = true
-            onFindUnvisitedVertex(currentVertex, currentEdge)
+            onEnterUnvisitedVertex(currentVertex, currentEdge)
             for (edge in graph.getOutgoingEdges(currentVertex)) {
                 val nextVertex = edge.to
                 if (!visitedVertices[nextVertex]!!) {
                     recursiveDfs(nextVertex, edge)
                 }
+                else {
+                    onFindVisitedVertex(nextVertex, edge)
+                }
                 visitedVertices[nextVertex] = false
-                onFindVisitedVertex(nextVertex, edge)
             }
+            onExitVisitedVertex(currentVertex, currentEdge)
         }
+
         recursiveDfs(startVertex, null)
     }
 }
