a1278eebf2b64c2c678d95e4779b0e83e4850fe5
Praytic <chernogorovvm@gmail.com>
2016-11-01 22:05:22 +0300

    Fix IsTree algorithm and add new tasks

--- graphs/src/main/kotlin/com/vchernogorov/graphs/algorithm/IsTree.kt
+++ graphs/src/main/kotlin/com/vchernogorov/graphs/algorithm/IsTree.kt
@@ -7,9 +7,15 @@ internal class IsTree<T>(val graph: Graph<T>): Algorithm<Boolean>(false) {
     override fun run() {
         if (graph.vertices.size - 1 === graph.edges.size) {
             var visitedCount = 0
+            var hasCycle = false
             val dfs = DepthFirstSearch(graph)
             dfs.onEnterUnvisitedVertex = { vertex, edge -> visitedCount++ }
+            dfs.onFindVisitedVertex = { v, e -> hasCycle = true }
             dfs.run()
+            if (hasCycle) {
+                result = false
+                return
+            }
             if (visitedCount === graph.vertices.size) {
                 result = true
             }
